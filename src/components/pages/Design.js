e306b2c11ce2fba20b6857aaf419938e418e2cb7
gourav6m17 <gouravinsaan@gmail.com>
2023-10-11 19:12:53 +0530

    Merge from Jashan to staging

--- src/components/pages/Design.js
+++ src/components/pages/Design.js
@@ -1,54 +1,142 @@
-import React from 'react'
-import Homeheader from '../Header'
-import Footer from '../Footer'
-import img1 from '../../assets/images/Design/Group 1.svg'
-import img2 from '../../assets/images/Design/Group 2.svg'
-import img3 from '../../assets/images/Design/Group 2236.svg'
-import img4 from '../../assets/images/Design/Group 3.svg'
-import img5 from '../../assets/images/Design/Group 4.svg'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Homeheader from "../Header";
+import Footer from "../Footer";
+import img1 from "../../assets/images/Design/Varanasi_Mehndi_Design_Images_Pictures_(Ideas)-transformed 1.svg";
+import img2 from "../../assets/images/Design/Arabic_Mehndi_Designs-removebg-preview 1.svg";
+import img3 from "../../assets/images/Design/Stylish_Mehndi_Designs-removebg-preview 1.svg";
+import img4 from "../../assets/images/Design/indo arabic 1.svg";
+import img5 from "../../assets/images/Design/Jaguar_Mhendi_Design-removebg-preview 1.svg";
+
+import img6 from "../../assets/images/Design/colored_tip_nails_1 1.svg";
+import img7 from "../../assets/images/Design/airbrushing_nails 1 (1).svg";
+import img8 from "../../assets/images/Design/airbrushing_nails 1 (2).svg";
+import img9 from "../../assets/images/Design/airbrushing_nails 1 (3).svg";
+import img10 from "../../assets/images/Design/colored_tip_nails_1 1 (1).svg";
+import img11 from "../../assets/images/Design/colored_tip_nails_1 1 (2).svg";
+
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { print, setData } from "../../Redux/Slices/FormSlice";
+import whatsapp from "../../assets/images/Frame 2219.svg";
 function Design() {
   const formData = useSelector((state) => state.FormData);
   const dispatch = useDispatch();
-
+  const bg = formData.Service === "Nail Art" ? 1 : undefined;
+  const designArray =
+    formData.Service === "Mehndi Design"
+      ? [
+          {
+            src: img1,
+            name: "Bridal Mehndi",
+            color: "#264653",
+          },
+          {
+            src: img2,
+            name: "Arabic Mehndi",
+          },
+          {
+            src: img3,
+            name: "Khafif Mehndi",
+          },
+          {
+            src: img4,
+            name: "Indo Arabic Mehndi",
+          },
+          {
+            src: img5,
+            name: "Tattoo Mehndi",
+          },
+        ]
+      : [
+          {
+            src: img6,
+            name: "Colored-Tip Nails",
+            color: "#264653",
+            textColor: "white",
+          },
+          {
+            src: img7,
+            name: "Freehand Nails",
+            color: "#2A9D8F",
+            textColor: "white",
+          },
+          {
+            src: img8,
+            name: "Air Brushing Nails",
+            color: "#E9C46A",
+            textColor: "black",
+          },
+          {
+            src: img9,
+            name: "Sticker Nails",
+            color: "#F4A261",
+            textColor: "black",
+          },
+          {
+            src: img10,
+            name: "Stencil Nails",
+            color: "#E76F51",
+            textColor: "white",
+          },
+          {
+            src: img11,
+            name: "Glitter Nails",
+            color: "#264653",
+            textColor: "white",
+          },
+        ];
   // Your changeHandler function
   function changeHandler(event) {
     // const { name, value, checked, type } = event.target;
-    const selectedDesign = event.target.getAttribute('name');
+    const selectedDesign = event.target.getAttribute("name");
     dispatch(setData({ Design: selectedDesign }));
-    dispatch(print()); 
+    dispatch(print());
   }
-    return (
-    <div>
-        <Homeheader/>
-        <div className="flex lg:flex-row flex-col justify-center mt-10 w-[100%] ">
-        {/* <div className="flex  space-x-5 "> */}
-        <div className="flex flex-col space-y-2 ">
-          <Link to='/price'>
-            <img src={img1} className="w-[90%] ml-10 justify-center" name="Bridal Mehndi" onClick={changeHandler}></img>    
+
+  return (
+    <div className="">
+      <a href="https://api.whatsapp.com/send?phone=918360741113&text=Hi,%20I%27ve%20seen%20your%20portfolio%20and%20want%20the%20booking">
+        <img
+          src={whatsapp}
+          width="120px"
+          height="70px"
+          className="fixed top-[50vh] right-0"
+        ></img>
+      </a>
+      <Homeheader line1="Choose Style" line2="5+ Mehendi Design" bg={bg} />
+      <div className="grid grid-rows-1 md:grid-cols-[_2fr_2fr] mx-[6.5%] gap-x-[7%]">
+        {designArray.map((design) => (
+          <Link to={formData.Service === "Nail Art" ? "/time" : "/price"}>
+            <div
+              className={`bg-[${design.color}]
+            grid grid-cols-[_2fr_1.5fr] lg:grid-cols-[_2.5fr_1.5fr] 
+            min-w-[310px] max-w-[650px] min-h-[210px]
+            max-h-[400px] w-full rounded-lg my-7
+            md:mt-[1.5rem]`}
+              onClick={changeHandler}
+            >
+              <span className="flex flex-col justify-center">
+                <h1
+                  className={`text-4xl sm:text-5xl font-Bell text-${design.textColor} px-5`}
+                >
+                  {design.name}
+                </h1>
+              </span>
+              <div className="w-full h-full relative">
+                <img
+                  src={design.src}
+                  name={design.name}
+                  className="w-full h-full object-cover relative left-0"
+                />
+              </div>
+            </div>
           </Link>
-          <Link to='/price'>
-            <img src={img2} className="w-[90%] ml-10" name="Arabic Mehndi" onClick={changeHandler}></img>    
-          </Link>
-          <Link to='/price'>
-            <img src={img3} className="w-[90%] ml-10" name="Khafif Mehndi" onClick={changeHandler}></img>    
-          </Link>
-          </div>
-          <div className="flex flex-col space-y-2">
-          <Link to='/price'>
-            <img src={img4} className="w-[90%] ml-10" name="Indo Arabic Mehndi" onClick={changeHandler}></img>    
-          </Link>
-          <Link to='/price'>
-            <img src={img5} className="w-[90%] ml-10" name="Tattoo Mehndi" onClick={changeHandler}></img>    
-          </Link>
-          </div>
-          {/* </div> */}
-        </div>
-        <Footer/>
+        ))}
+      </div>
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Design
+export default Design;
--- src/components/pages/Price.js
+++ src/components/pages/Price.js
@@ -5,7 +5,6 @@ import HandLengthImage from "../../assets/images/Price/Group 2237.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { print, setData } from "../../Redux/Slices/FormSlice";
 import { Link } from "react-router-dom";
-// import whatsapp from "../../assets/images/Frame 2219.svg";
 import whatsapp from "../../assets/images/Frame 2219.svg";
 function Price() {
   const [SelectedPrice, setSelectedPrice] = useState(null);
@@ -30,15 +29,11 @@ function Price() {
     var price = 0;
     switch (selectedLength) {
       case "Elbow":
-    switch (selectedLength) {
-      case "Elbow":
         price = 253;
         break;
       case "Wrist":
-      case "Wrist":
         price = 252;
         break;
-      case "Palm":
       case "Palm":
         price = 251;
         break;
@@ -52,10 +47,7 @@ function Price() {
       <a href="https://api.whatsapp.com/send?phone=918360741113&text=Hi,%20I%27ve%20seen%20your%20portfolio%20and%20want%20the%20booking">
         <img src={whatsapp} width="120px" height="70px" className="fixed top-[50vh] right-0"></img>
       </a>
-      {/* <a href="https://api.whatsapp.com/send?phone=918360741113&text=Hi,%20I%27ve%20seen%20your%20portfolio%20and%20want%20the%20booking">
-        <img src={whatsapp} width="120px" height="70px" className="fixed top-[50vh] right-0"></img>
-      </a> */}
-      <Homeheader line1="Choose Style" line2="5+ Mehendi Design" line1="Choose Style" line2="5+ Mehendi Design" />
+      <Homeheader line1="Choose Style" line2="5+ Mehendi Design" />
       <div className="flex flex-row justify-between">
         {/* left */}
         <div className="border border-black ml-[10%]">
